7ea0fd6d58571dfdc2d96eb72f939ce1003fb0de
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-04-26 15:28:23 +0200

    Ajout de commentaires et de la doc.

--- README.md
+++ README.md
@@ -1,11 +1,72 @@
 # app-meteo-v2
-Mise a jour de ma première app meteo en typescrit et avec un code plus propre.
 
-## Next step
+Mise a jour de ma première app meteo que j'avais réalisée avant ma formation chez O'clock.
 
-- Constuire un nouveau bloc en html css pour accueillir les prédictions à 4 jours
-- https://openweathermap.org/api/hourly-forecast
-- Implémenter les fonctions et fetch pour aller chercher les infos
-- Implémenter les fonctions et insertions dans le dom
+> Update of my first weather app relised before my training from O'Clock school.
 
-- Formulaire avec propositions de ville à la saisie ?
+Mon objectif ici était de la refaire avec un code plus propre et en typescript tout en profitant de l'occasion pour ajouter quelques features comme l'affichage les prévisions des prochaines heures et prochains jours.
+
+> My goal here was to remake this app properly with typescript and trying to add some features like some  forecast.
+
+---
+
+## Stack
+
+Projet développé avec ViteJS en TypeScript Vanilla.
+L'API météo utilisée est [OpenWeatherMap](https://openweathermap.org/)
+
+> This project was made with ViteJS in Vanilla TypeScript
+> The weather API used here is [OpenWeatherMap](https://openweathermap.org/)
+
+---
+
+## Project Setup
+
+### Install
+
+```sh
+npm install
+```
+
+ou
+
+```sh
+yarn
+```
+
+### ApiKey
+
+Pour que l'appel à l'API fonctionne, vous devez utilisez une variable d'environnement dans un fichier `.env` à la racine du projet
+
+> You need to install environnement variable file `.env` at the root of the project.
+
+```dotenv
+VITE_API_KEY: yourkey
+```
+
+Pour avoir votre propre clé API vous devez vous créer un compte sur le site de [OpenWeatherMap](https://openweathermap.org/)
+> To get you API key, you need to create an account to the website : [OpenWeatherMap](https://openweathermap.org/)
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+ou
+
+```sh
+yarn dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+ou
+
+```sh
+yarn build
+```
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { IApp, IData, IDayForecast, IArrayData } from './@types/app';
+import { IApp, IData, IDayForecast } from './@types/app'; // Import du typage
 import './style.scss';
-const apiKey = import.meta.env.VITE_API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY; // import de la clé API dans le fichier .env à la racine du projet
 
 const app: IApp = {
   months: [
@@ -35,6 +35,7 @@ const app: IApp = {
   cityForm: document.getElementById('city-form')! as HTMLFormElement,
   cityInput: document.getElementById('city')! as HTMLInputElement,
   notificationsDiv: document.getElementById('notifications')! as HTMLDivElement,
+  // init() est la fonction d'initialisation. Elle pose l'écouteur d'événement sur le formulaire et affiche la météo de `app.actualCity`, initialisée sur Paris.
   init: async () => {
     console.log("Initialisation de l'application en cours...");
     app.cityForm.addEventListener('submit', app.handleFormSubmit);
@@ -42,8 +43,10 @@ const app: IApp = {
     console.log('Application initialisée.');
     app.notify('Bienvenue, veuillez choisir votre ville');
   },
+  // refreshApp() est la fonction mère qui exécture les foncitons nécessaire pour afficher la météo de la ville contenu dans app.actualCity
   refreshApp: async () => {
     console.log('Réception du formulaire...');
+    // D'abord elle récupère la latitude et longitude de la ville souhaitée
     const actualCityLoc = await app.getLocation();
     if (
       (actualCityLoc.lat === 0 && actualCityLoc.lon === 0) ||
@@ -52,24 +55,28 @@ const app: IApp = {
     ) {
       app.notify("Aucune ville n'a été trouvée à ce nom... 😢", 5, 'error');
     } else {
-      // Météo actuelle
+      // Ici, elle récupère la météo actuelle
       const actualWheather = await app.getWeather(actualCityLoc);
+      // Et l'affiche dans le dom
       app.showWeatherInDom(actualWheather);
 
-      // Pérvisions météo
+      // Ici, elle récupère les prévisions météos
       const foreCastWeather = await app.getForecast(actualCityLoc);
+      // Affiche les prévisions des prochaines heures dans le DOM
       app.showForecastHoursInDom(foreCastWeather);
+      // Affiche les prévisions des prochains jours dans le DOM
       app.showForecastDaysInDom(foreCastWeather);
 
-      // Reset de l'input
+      // Reset de l'input du formulaire
       app.cityInput.value = '';
 
-      // Affichage d'une notification seulement lors d'un submit utilisateur
+      // Affichage d'une notification seulement lors d'un submit utilisateur et non au premier chargement de la page
       app.counterRefresh > 0 &&
         app.notify(`Voici la météo pour ${app.actualCity} 😄`, 5, 'nice');
       app.counterRefresh++;
     }
   },
+  // getLocation() est la fonction qui permet de récupérer la longitude et latitude d'une ville grâce a l'URL API fournis par Openweathermap
   getLocation: async () => {
     console.log(
       `Récupération des coordonnées en cours pour la ville de ${app.actualCity}...`
@@ -84,16 +91,12 @@ const app: IApp = {
         lat: 0,
         lon: 0,
       };
-      if (json.length > 1) {
-        actualCity.lat = json[0].lat;
-        actualCity.lon = json[0].lon;
-        return actualCity;
-      } else if (json.length === 1) {
-        actualCity.lat = json.lat;
-        actualCity.lon = json.lon;
+      if (json.length === 0) {
+        console.log('Ville non trouvée...');
         return actualCity;
       } else {
-        console.log('Ville non trouvée...');
+        actualCity.lat = json[0].lat;
+        actualCity.lon = json[0].lon;
         return actualCity;
       }
     } catch (error: unknown) {
@@ -101,6 +104,7 @@ const app: IApp = {
       return;
     }
   },
+  // getWeather() est la fonction qui permet de récupérer auprès de l'API la météo du jour grâce aux coordonées passées en paramètre.
   getWeather: async (actualCityLoc) => {
     const { lat, lon } = actualCityLoc;
     console.log(
@@ -112,7 +116,6 @@ const app: IApp = {
       );
       if (response.status !== 200) throw new Error('Problème API');
       const json = await response.json();
-      console.log(json);
       console.log('Données réceptionnées');
       return json;
     } catch (error) {
@@ -120,6 +123,7 @@ const app: IApp = {
       return;
     }
   },
+  // getForecast() est la fonction qui permet de récupèrer auprès de l'API les 40 prochaines prévisions météo par tranche de 3h grâce aux coordonées passées en paramètre.
   getForecast: async (actualCityLoc) => {
     const { lat, lon } = actualCityLoc;
     console.log(
@@ -139,20 +143,21 @@ const app: IApp = {
       return;
     }
   },
+  // showWeatherInDom() s'occupe de générer des éléments HTML et d'afficher les données de la météo actuelle (passée en paramètre) dans le DOM.
   showWeatherInDom: (data) => {
     console.log('Affichage des données en cours...');
     app.actualCityTitle.textContent = `Actuellement à ${app.actualCity}`;
     const weatherDescription = data.weather[0].description;
     app.actualWeatherTxt.textContent =
-      weatherDescription[0].toUpperCase() + weatherDescription.slice(1);
+      weatherDescription[0].toUpperCase() + weatherDescription.slice(1); // Met le nom de la ville avec la première lettre en majuscule.
     app.actualWeatherImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    app.actualTempTxt.textContent = `${data.main.temp.toFixed(1)}°C`;
+    app.actualTempTxt.textContent = `${data.main.temp.toFixed(1)}°C`; // Arrondi la température à 1 chiffre après la virgule.
     app.actualFeelTxt.textContent = `Ressenti : ${data.main.feels_like.toFixed(
       1
     )}°C`;
     app.actualWindTxt.textContent = `${(data.wind.speed * 3.6).toFixed(
       2
-    )} km/h`;
+    )} km/h`; // Converti la vitesse du vends de m/s en km/h
     app.actualHumidityTxt.textContent = `${Math.round(
       data.main.humidity
     )}% d'humitité`;
@@ -167,8 +172,9 @@ const app: IApp = {
       'h' +
       new Date(data.sys.sunset * 1000).getMinutes();
     app.actualSunTxt.textContent = `Levé à ${sunrise} couché à ${sunset}`;
-    // Actualisation du background
+    // Actualisation du background en fonction de la météo
     if (data.weather[0].id.toString()[0] === '7') {
+      // Les météos de code 7xx correspondent à la brume mais possède pleins de noms différents. J'ai trouvé plus pertinent d'afficher le même background pour tous les codes commencant par 7.
       document.body.style.background = `url('../img/Fog.jpg') no-repeat center/cover fixed`;
     } else {
       document.body.style.background = `url('../img/${data.weather[0].main}.jpg') no-repeat center/cover fixed`;
@@ -176,19 +182,25 @@ const app: IApp = {
 
     console.log('Données actualisées dans le DOM.');
   },
+  // showForecastHoursInDom() reçoit les prévisions par tranche de 3h, récupère les 3 premières prévisions et les affiches dans le DOM.
   showForecastHoursInDom: (data) => {
     console.log('Affichage des prévisions des 9 prochaines heures en cours...');
-    app.forecastHoursDiv.textContent = '';
+    app.forecastHoursDiv.textContent = ''; // vide la div container.
+
     for (let i = 0; i < 3; i++) {
+      // On récupère les trois premières prévision via l'index [i]
       const oneForecast = data[i];
+      // Div englobante de class card
       const divContainer = document.createElement('div');
       divContainer.className = 'card';
+      // Div pour la date
       const divHours = document.createElement('div');
       divHours.className = 'date';
       const thisDate = new Date(oneForecast.dt * 1000);
       divHours.textContent = `${thisDate.getDate()}-${
         app.months[thisDate.getMonth()]
       } ${thisDate.getHours()}h`;
+      // Div pour la météo
       const divWeather = document.createElement('div');
       divWeather.className = 'card__weather';
       const icon = document.createElement('img');
@@ -198,24 +210,29 @@ const app: IApp = {
       divTemp.textContent = `${oneForecast.main.temp.toFixed(1)}°C`;
       divWeather.append(icon, divTemp);
       divContainer.append(divHours, divWeather);
+      // Mise en DOM
       app.forecastHoursDiv.appendChild(divContainer);
     }
   },
-  // TODO: Afficher prévisions matin et aprem par jour
+  // showForecastDaysInDom() reçoit les prévisions par tranche de 3h et affiche les prévisions des 4 prochains jours au format matin / après-midi (voir détail dans la fonction)
   showForecastDaysInDom: (data) => {
     console.log('Affichage des prévisions des 9 prochaines heures en cours...');
-    app.forecastDaysDiv.textContent = '';
-    for (let i = 1; i < 6; i++) {
+    app.forecastDaysDiv.textContent = ''; // vide la div container.
+
+    for (let i = 1; i < 5; i++) {
+      // le i incrémenté sera le jour j+i passé en parramètre pour la fonction getForecastPerDay()
       const oneDayForecast = app.getForecastPerDay(i, data);
 
-      // Container
+      // Div englobante
       const divContainer = document.createElement('div');
       divContainer.className = 'card';
+
+      // Div date
       const divDate = document.createElement('div');
       divDate.className = 'date';
       divDate.textContent = oneDayForecast.date;
 
-      // Bloc matin
+      // Div englobante pour la météo du matin
       const divMorning = document.createElement('div');
       const divTitleMorning = document.createElement('div');
       divTitleMorning.textContent = 'Matin';
@@ -229,7 +246,7 @@ const app: IApp = {
       divWeatherMorning.append(iconMorning, divTempMorning);
       divMorning.append(divTitleMorning, divWeatherMorning);
 
-      // Bloc après-midi
+      // Div englobante pour la météo de l'après midi
       const divAfternoon = document.createElement('div');
       const divTitleAfternoon = document.createElement('div');
       divTitleAfternoon.textContent = 'Après-midi';
@@ -246,13 +263,14 @@ const app: IApp = {
       app.forecastDaysDiv.appendChild(divContainer);
     }
   },
+  // getForecastPerDay() est une fonction générique qui permet dé récupérer les prévisions pour le jour 'aujourd'hui + day' dans le tableau de prévisions 'data'
   getForecastPerDay: (day, data) => {
     console.log(`Récupération des prévisions pour J+${day}`);
-    // On détermine la bonne date à J+1
+    // On détermine la bonne date à J+1 a partir de la date du jour
     const today = new Date();
     const wantedDay = new Date();
     wantedDay.setDate(today.getDate() + day);
-    // On récupère les prévisions qui concerne la bonne date entre 09h et 12h
+    // On récupère les prévisions qui concerne wantedDay entre 09h et 12h
     const morgningForecast = data.find((date: IData) => {
       const thisDate = new Date(date.dt * 1000);
       return (
@@ -261,16 +279,16 @@ const app: IApp = {
         thisDate.getHours() < 12
       );
     });
-    // On récupère les prévisions qui concerne la bonne date entre 13h et 19h
+    // On récupère les prévisions qui concerne wantedDay entre 13h et 19h
     const afternoonForecast = data.find((date: IData) => {
       const thisDate = new Date(date.dt * 1000);
       return (
         thisDate.getDate() === wantedDay.getDate() &&
         thisDate.getHours() > 13 &&
-        thisDate.getHours() < 17
+        thisDate.getHours() < 18
       );
     });
-    // On construit l'objet de réponse et on le return
+    // On return l'objet avec les infos dont on a besoin pour générer l'élement dans la fonction showForecastDaysInDom()
     return {
       date: `${wantedDay.getDate()}-${app.months[wantedDay.getMonth()]}`,
       am: {
@@ -285,17 +303,17 @@ const app: IApp = {
       },
     } as IDayForecast;
   },
-  //TODO: pb avec encoreuricomponent impossible de saisir des noms composé comme villers-sur-mer
+  // handleSubmitForm() gère le submit du formulaire
   handleFormSubmit: (e) => {
     e.preventDefault();
     console.log('Soumission du formulaire. Récupération des données...');
     const data = new FormData(app.cityForm);
-    const newCity: any = data.get('city')?.toString()!;
-    const newCityConverted: any = encodeURIComponent(newCity);
-    console.log(newCityConverted);
+    const newCity: any = data.get('city')?.toString()!; // Récupération de la valeur de l'input
+    const newCityConverted: any = encodeURIComponent(newCity); // filtre la valeur de l'input pour éviter les failles XSS
     if (isNaN(newCityConverted)) {
-      app.actualCity = newCityConverted;
-      app.refreshApp();
+      // On vérifie également si la valeur saisie n'est pas juste un nombre
+      app.actualCity = newCityConverted; // On affecte la nouvelle ville a notre variable app.actualCity()
+      app.refreshApp(); // Et on lance le refresh des infos du DOM avec la météo de la nouvelle ville
     } else {
       app.notify(
         'Vous avez saisit un nombre ! Veuillez saisir une ville française 😄',
@@ -304,6 +322,7 @@ const app: IApp = {
       );
     }
   },
+  // notify() est une fonction générique qui permet de générer une notification pour l'utilisateur notamment lorsque la ville saisie n'est pas reconnu.
   notify: (message, timout = 3, theme = 'neutral') => {
     app.notificationsDiv.className = theme;
     app.notificationsDiv.textContent = message;
